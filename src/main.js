713c57ab32491c7799cf99acaf78a407b79a00b0
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-10-19 15:15:16 -0400

    Updated the method to call the dafd plugin on 3DuF

--- src/main.js
+++ src/main.js
@@ -23,3 +23,18 @@ export function openDesignWithParamChanges(fileurl, componentname, params) {
     console.log("DAFD Link:", baseurl);
     window.open(baseurl, '_blank');
 }
+
+/**
+ * Opens the design on 3DuF.org with the specified in the DAFD mode
+ * @param fileurl full public url for 3DuF to open the file
+ * @param componentname component name of the component that needs to change
+ * @param params Javascript object key value pairs of parameters that need to change
+ */
+export function openDesignWithDAFDParams(params) {
+    // let baseurl = new URL("https://3duf.org/");
+    let baseurl = new URL("file:///Users/krishna/Documents/3DuF/index.html");
+    console.log(JSON.stringify(params));
+    baseurl.searchParams.append('dafdparams', (JSON.stringify(params)));
+    console.log("DAFD Link:", baseurl);
+    window.open(baseurl, '_blank');
+}
